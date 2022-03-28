819da03f481fa7bfe0c0a060e7ef222ce02b89c6
12-coder <62683187+12-coder@users.noreply.github.com>
2022-03-28 13:41:16 +0530

    Update Employee.java
    modify

--- src/main/java/com/springrest/entity/Employee.java
+++ src/main/java/com/springrest/entity/Employee.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Employees")
+@Table(name="Employee")
 public class Employee {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
